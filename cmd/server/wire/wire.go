97284158135d45087a1c65b255f5abc88c4e5abe
dnslin <i@dnsl.in>
2024-11-20 10:40:46 +0800

    feat: init project

---
+++ cmd/server/wire/wire.go
@@ -0,0 +1,65 @@
+//go:build wireinject
+// +build wireinject
+
+package wire
+
+import (
+	"novel-site-backend/internal/handler"
+	"novel-site-backend/internal/repository"
+	"novel-site-backend/internal/server"
+	"novel-site-backend/internal/service"
+	"novel-site-backend/pkg/app"
+	"novel-site-backend/pkg/jwt"
+	"novel-site-backend/pkg/log"
+	"novel-site-backend/pkg/server/http"
+	"novel-site-backend/pkg/sid"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var repositorySet = wire.NewSet(
+	repository.NewDB,
+	//repository.NewRedis,
+	repository.NewRepository,
+	repository.NewTransaction,
+	repository.NewUserRepository,
+)
+
+var serviceSet = wire.NewSet(
+	service.NewService,
+	service.NewUserService,
+)
+
+var handlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
+
+var serverSet = wire.NewSet(
+	server.NewHTTPServer,
+	server.NewJob,
+)
+
+// build App
+func newApp(
+	httpServer *http.Server,
+	job *server.Job,
+	// task *server.Task,
+) *app.App {
+	return app.NewApp(
+		app.WithServer(httpServer, job),
+		app.WithName("demo-server"),
+	)
+}
+
+func NewWire(*viper.Viper, *log.Logger) (*app.App, func(), error) {
+	panic(wire.Build(
+		repositorySet,
+		serviceSet,
+		handlerSet,
+		serverSet,
+		sid.NewSid,
+		jwt.NewJwt,
+		newApp,
+	))
+}
---
+++ internal/service/service.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"novel-site-backend/internal/repository"
+	"novel-site-backend/pkg/jwt"
+	"novel-site-backend/pkg/log"
+	"novel-site-backend/pkg/sid"
+)
+
+type Service struct {
+	logger *log.Logger
+	sid    *sid.Sid
+	jwt    *jwt.JWT
+	tm     repository.Transaction
+}
+
+func NewService(
+	tm repository.Transaction,
+	logger *log.Logger,
+	sid *sid.Sid,
+	jwt *jwt.JWT,
+) *Service {
+	return &Service{
+		logger: logger,
+		sid:    sid,
+		jwt:    jwt,
+		tm:     tm,
+	}
+}
